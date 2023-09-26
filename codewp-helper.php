427e92cbe12626fcae6cf947b0bcf8463adb8e29
Galatan Ovidiu <galatanovidiu@gmail.com>
2023-09-26 16:05:45 +0300

    api_key_form and codewp_server have been added to CWPAI_SETTINGS
    api-key-functions.php included
    CWPAI_API_SERVER constant added. This can be rewritten on wp-config.php for local testing

--- codewp-helper.php
+++ codewp-helper.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * Plugin Name:       CodeWP Helper
  * Plugin URI:        https://github.com/wpai/codewp-helper
@@ -15,21 +14,27 @@
  * Requires at least: 5.8
  * Tested up to:      6.1.1
  * Requires PHP:      7.1
+ *
+ * @package           CodeWP Helper
  */
 
 namespace CodeWPHelper;
 
-if (!defined('ABSPATH')) {
+if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-define('CWPAI_SETTINGS_PATH', \plugin_dir_path(__FILE__));
-define('CWPAI_SETTINGS_URL', \plugins_url('/', __FILE__));
-define('CWPAI_SETTINGS_PLUGIN_FILE', __FILE__);
-define('CWPAI_SETTINGS_PLUGIN_DIR', __DIR__);
-define('CWPAI_SETTINGS_VERSION', '0.0.1');
+define( 'CWPAI_SETTINGS_PATH', \plugin_dir_path( __FILE__ ) );
+define( 'CWPAI_SETTINGS_URL', \plugins_url( '/', __FILE__ ) );
+define( 'CWPAI_SETTINGS_PLUGIN_FILE', __FILE__ );
+define( 'CWPAI_SETTINGS_PLUGIN_DIR', __DIR__ );
+define( 'CWPAI_SETTINGS_VERSION', '0.0.1' );
+if ( ! defined( 'CWPAI_API_SERVER' ) ) {
+	define( 'CWPAI_API_SERVER', 'https://codewp.ai' ); // TODO: change this to the production server.
+}
 
 require 'includes/utils/register-ajax-method.php';
+require 'includes/utils/api-key-functions.php';
 require 'includes/utils/enqueue-scripts-from-asset-file.php';
 require 'includes/admin-page.php';
 require 'includes/plugin-links.php';
@@ -37,10 +42,12 @@ require 'includes/ajax.php';
 
 add_filter(
 	'cwpai_settings_variables',
-	function ($variables) {
-		$variables['notice_hidden'] = get_option('cwpai-settings/notice_hidden', false);
-		$variables['simple_form'] = get_option('cwpai-settings/simple_form', []);
-		$variables['repeated_form'] = get_option('cwpai-settings/repeated_form', []);
+	function ( $variables ) {
+		$variables['codewp_server'] = CWPAI_API_SERVER;
+		$variables['notice_hidden'] = get_option( 'cwpai-settings/notice_hidden', false );
+		$variables['simple_form']   = get_option( 'cwpai-settings/simple_form', array() );
+		$variables['repeated_form'] = get_option( 'cwpai-settings/repeated_form', array() );
+		$variables['api_key_form']  = wpai_get_api_key_form_data();
 
 		return $variables;
 	}
